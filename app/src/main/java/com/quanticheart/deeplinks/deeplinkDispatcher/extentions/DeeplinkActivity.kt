295310a0881c0a2e673c3604037f9b8a1ceea94c
Jonn Alves <jonatas.alsantos@jsl.com.br>
2021-11-19 13:18:35 -0300

    added validation for handle deeplink in DeeplinkActivity.kt

--- app/src/main/java/com/quanticheart/deeplinks/deeplinkDispatcher/extentions/DeeplinkActivity.kt
+++ app/src/main/java/com/quanticheart/deeplinks/deeplinkDispatcher/extentions/DeeplinkActivity.kt
@@ -1,23 +1,32 @@
 package com.quanticheart.deeplinks.deeplinkDispatcher.extentions
 
 import android.os.Bundle
+import android.util.Log
 import android.webkit.URLUtil
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.deeplinkdispatch.DeepLinkHandler
+import com.quanticheart.deeplinks.BuildConfig
 
 @DeepLinkHandler(DeepLinkModule::class)
 class DeeplinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DeepLinkDelegate(DeepLinkModuleRegistry()).dispatchFrom(this)?.let {
-            if (!it.isSuccessful) {
-                val url = intent?.data?.toString() ?: ""
+        try {
+            DeepLinkDelegate(DeepLinkModuleRegistry()).dispatchFrom(this)?.let {
+                if (!it.isSuccessful) {
+                    val url = intent?.data?.toString() ?: ""
 //                Patterns.WEB_URL.matcher(intent?.data?.toString() ?: "").matches().toString()
-                if (URLUtil.isValidUrl(url)) {
-                    browse(url).start(this)
+                    if (URLUtil.isValidUrl(url)) {
+                        browse(url).start(this)
+                    }
                 }
             }
+        } catch (iae: IllegalArgumentException) {
+            if (BuildConfig.DEBUG) {
+                Log.e("Deep links", "Invalid URI $iae")
+            }
+        } finally {
+            finish()
         }
-        finish()
     }
 }
